8d7806a2371f0f5f3e97c66329385787ddc34dd7
Ben McShane <benjaminmartinmc@gmail.com>
2022-08-01 16:33:19 -0600

    add flavor

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "This content has been added by outside forces. Fear our power, it is too late to stop us now.";
 document.querySelector("body").appendChild(h2);
